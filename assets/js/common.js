83c8452483ae5277af52ab2c19a191e720521ff7
Mike Simms <msimms01@gmail.com>
2025-11-07 14:46:31 -0500

    Added times in half hour increments for meeting schedules.

--- assets/js/common.js
+++ assets/js/common.js
@@ -15,6 +15,12 @@ function unix_time_to_local_date_string(unix_time) {
     return date.toLocaleString().split(',')[0];
 }
 
+/// @function unix_time_to_string
+function unix_time_to_string(unix_time) {
+    let date = new Date(unix_time);
+    return date.toLocaleString().split(',')[1].trim();
+}
+
 /// @function unix_time_to_utc_string
 function unix_time_to_utc_string(unix_time) {
     let date = new Date(unix_time);
